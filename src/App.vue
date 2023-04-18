2d6d3b35ecfc1cd728884b8716bbcf533a5dca24
markb <markbuchholz8@gmail.com>
2023-04-17 21:53:36 -0600

    made all files

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <music-selection></music-selection>
+  </div>
+</template>
+
+<script>
+import MusicSelection from '@/components/MusicSelection.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MusicSelection
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
